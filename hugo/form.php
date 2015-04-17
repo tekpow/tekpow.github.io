d7b2fc9791a5e6003c295b4b6c88afba1c28c7b0
marceau bernelin <bernel_m@epitech.eu>
2015-04-17 13:15:54 +0000

    catch_summ

--- hugo/form.php
+++ hugo/form.php
@@ -14,7 +14,15 @@
   <body>
     <?php
        $player = $_POST['player'];
-       echo $player; 
-   ?>
+       echo $player;
+
+       $url = 'https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/' . $player . '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41';
+       $ch = curl_init($url);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+       $infos = curl_exec($ch);
+       curl_close($ch);
+
+       echo $infos;
+    ?>
   </body>
 </html>
