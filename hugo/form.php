62b1cec0dc166874bb0fec282bb98cce107134ce
villev_h <hugo.villevieille@epitech.eu>
2015-04-20 17:41:42 +0000

    tamer php

--- hugo/form.php
+++ hugo/form.php
@@ -7,50 +7,108 @@
                    |___/                                                 -->
 
 <html lang="en">
-	<title>LolPow</title>
+  <title>LolPow</title>
+  <head>
+    <meta charset="utf-8" />
 
-	<head>
-	</head>
+    <!-- Cascading Stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="php.css" rel="stylesheet" type="text/css">
+    <link href="css/dropdown.css" rel="stylesheet" type="text/css">
+    <link href="css/navbar.css" rel="stylesheet" type="text/css">
 
-	<body>
-		<?php
-			$region = 'euw';
-			$player = $_POST['player'];
-			$summId = 28954491;
-			$api_key = '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41';
+    <!-- Site Icon -->
+    <link href="/res/ico/Tray_32.png" rel="shortcut icon">
 
-			$v1_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.2/';
-			$v1_3 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.3/';
-			$v1_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.4/';
-			$v2_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.2/';
-			$v2_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.4/';
-			$v2_5 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.5/';
-	   
-			function askApi($url)
-			{
-				$ch = curl_init($url);
-				curl_setopt($ch, CurlOPT_RETURNTRANSFER, 1);
-				$infos = curl_exec($ch);
-				curl_close($ch);
-				return $infos;
-			}
-			
-			function getSummonerId($v1_4, $player, $api_key)
-			{
-				$url = $v1_4 . 'summoner/by-name/' . $player . $api_key;
-				echo $url;
-				$data = askApi($url);
-				echo $data;
-			}
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </head>
 
-			function getMatchHistory($v2_2, $summId, $api_key)
-			{
-				$url = $v2_2 . 'matchhistory/' . $summId . $api_key;
-				echo $url;
-				$data = askApi($url);
-				echo $data;
-			}
-			getSummonerId($v1_4, $player, $api_key);
-		?>
-	</body>
-</html>
+  <div class="all">
+
+    <!-- NavBar -->
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+
+        <div class="navbar-header">
+          <a class="navbar-brand" href="file:///home/villev_h/HTML/website/hugo/my_website.html">
+            <img src="../res/ico/logo2.png">
+          </a>
+        </div>
+
+        <form class="navbar-form navbar-right" method="post" action="form.php">
+          <div class="form-group">
+
+            <div class="btn-group" data-toggle="buttons">
+              <label class="btn btn-primary active">
+                <input type="radio" name="region" value="euw" id="option1" autocomplete="off" checked>\
+ EUW
+              </label>
+              <label class="btn btn-primary">
+                <input type="radio" name="region" value="na" id="option2" autocomplete="off"> NA
+              </label>
+            </div>
+
+            <input type="text" class="form-control" name="player" placeholder="Search Summoner">
+            <button title="Get Stats" type="submit" class="btn btn-default">Go!</button>
+          </div>
+        </form>
+      </div>
+    </nav>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+ <?php	 
+	                $region = 'euw';
+                        $player = $_POST['player'];
+                        $summId = 28954491;
+                        $api_key = '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41';
+
+                        $v1_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.2/';
+                        $v1_3 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.3/';
+                        $v1_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.4/';
+                        $v2_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.2/';
+                        $v2_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.4/';
+                        $v2_5 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.5/';
+
+                        function askApi($url)
+                        {
+                                $ch = curl_init($url);
+                                curl_setopt($ch, CurlOPT_RETURNTRANSFER, 1);
+                                $infos = curl_exec($ch);
+                                curl_close($ch);
+                                return $infos;
+                        }
+
+                        function getSummonerId($v1_4, $player, $api_key)
+                        {
+                                $url = $v1_4 . 'summoner/by-name/' . $player . $api_key;
+                                echo $url;
+                                $data = askApi($url);
+                                echo $data;
+                        }
+
+                        function getMatchHistory($v2_2, $summId, $api_key)
+                        {
+                                $url = $v2_2 . 'matchhistory/' . $summId . $api_key;
+                                echo $url;
+                                $data = askApi($url);
+                                echo $data;
+                        }
+                        getSummonerId($v1_4, $player, $api_key);
+                ?>
+      <span class="col-md-4">
+      </span>
+      <span class="col-md-4">
+        <h1 class="text-center">LoL Scouter</h1>
+        <P class="text-center"> Enter a summoner name and we will fetch the stats for the Account</p>
+      </span>
+      <span class="col-md-4">
+      </span>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer">
+    </div>
+  </div>
