435dff651e0d71b8c1c73ae2747aed83c95fccce
Marceau Bernelin <marceau.bernelin@epitech.eu>
2015-04-24 12:58:57 +0200

    class lolPow

--- form.php
+++ form.php
@@ -65,71 +65,94 @@
     		<h1 class="text-center">LoL Scouter</h1>
     		<p class="text-center"> Enter a summoner name and we will fetch the stats for the Account</p>
         <p>
-      <?php  
-      $region = $_POST['region'];
-      $player = strtolower($_POST['player']);
-      $api_key = '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41';
+      <?php
 
-      $v1_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.2/';
-      $v1_3 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.3/';
-      $v1_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v1.4/';
-      $v2_2 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.2/';
-      $v2_4 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.4/';
-      $v2_5 = 'https://' . $region . '.api.pvp.net/api/lol/' . $region . '/v2.5/';
-
-      switch ($region)
+      class lolPow
       {
-        case 'euw':
-        $platformId = 'EUW1';
-        break;
+        public $region;
+        public $player;
+        public $api_key;
+        public $onetwo;
+        public $onethree;
+        public $onefour;
+        public $twotwo;
+        public $twofour;
+        public $twofive;
+        public $summId;
 
-        case 'na':
-        $platformId = 'NA1';
-        break;
-
-        default:
-        break;
-      }
-
-      function askApi($url)
-      {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $infos = curl_exec($ch);
-        curl_close($ch);
-        return $infos;
-      }
-
-      function getSummonerId($v1_4, $player, $api_key)
-      {
-        $url = $v1_4 . 'summoner/by-name/' . $player . $api_key;
-        $data = askApi($url);
-        $result = json_decode($data);
-        return $result->$player->id;
-      }
-
-      function spectate($region, $platformId, $summId, $api_key)
-      {
-        $data = askApi('https://' . $region . '.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/' . $platformId . '/' . $summId . $api_key);
-        $result = json_decode($data);
-        foreach ($result->participants as $name)
+        public function __construct($region, $player, $api_key)
         {
-          echo $name->summonerName . " - ";
-          $champ_data = askApi('https://global.api.pvp.net/api/lol/static-data/' . $region . '/v1.2/champion/' . $name->championId . $api_key);
-          $champ = json_decode($champ_data);
-          echo $champ->name . " - ";
-          $summSpell = askApi('https://global.api.pvp.net/api/lol/static-data/' . $region .'/v1.2/summoner-spell/' . $name->spell1Id . $api_key);
-          $summSpell1 = json_decode($summSpell);
-          $summSpell = askApi('https://global.api.pvp.net/api/lol/static-data/' . $region .'/v1.2/summoner-spell/' . $name->spell2Id . $api_key);
-          $summSpell2 = json_decode($summSpell);
-          echo $summSpell1->name . " & " . $summSpell2->name . " / \n";
+          $this->region = $region;
+          $this->player = $player;
+          $this->api_key = $api_key;
+          $this->onetwo = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.2/';
+          $this->onethree = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.3/';
+          $this->onefour = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.4/';
+          $this->twotwo = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.2/';
+          $this->twofour = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.4/';
+          $this->twofive = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.5/';
         }
+  
+        public function askApi($url)
+        {
+          $ch = curl_init($url);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          $infos = curl_exec($ch);
+          curl_close($ch);
+          return $infos;
+        }
+
+        public function getSummonerId()
+        {
+          $url = $this->onefour . 'summoner/by-name/' . $this->player . $this->api_key;
+          $data = $this->askApi($url);
+          $result = json_decode($data);
+          $player = $this->player;
+          $this->summId = $result->$player->id;
+        }
+
+        public function regionToSpectate()
+        {
+          $this->getSummonerId();
+          switch ($this->region)
+          {
+            case 'euw':
+            $platformId = 'EUW1';
+            break;
+
+            case 'na':
+            $platformId = 'NA1';
+            break;
+
+            default:
+            break;
+          }
+          return ('https://' . $this->region . '.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/' . $platformId . '/' . $this->summId . $this->api_key);
+        }
+
+        public function spectate()
+        {
+          $url = $this->regionToSpectate();
+          $data = $this->askApi($url);
+          $result = json_decode($data);
+          foreach ($result->participants as $name)
+          {
+            echo $name->summonerName . " - ";
+            $champ_data = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region . '/v1.2/champion/' . $name->championId . $this->api_key);
+            $champ = json_decode($champ_data);
+            echo $champ->name . " - ";
+            $summSpell = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region .'/v1.2/summoner-spell/' . $name->spell1Id . $this->api_key);
+            $summSpell1 = json_decode($summSpell);
+            $summSpell = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region .'/v1.2/summoner-spell/' . $name->spell2Id . $this->api_key);
+            $summSpell2 = json_decode($summSpell);
+            echo $summSpell1->name . " & " . $summSpell2->name . " / \n";
+          }
+        }  
       }
-
-      $summId = getSummonerId($v1_4, $player, $api_key);
-      spectate($region, $platformId, $summId, $api_key);
       
-
+      $stats = new lolPow($_POST['region'], strtolower($_POST['player']), '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41');
+      $stats->spectate();
+      
       ?>
         </p>
     		</span>
