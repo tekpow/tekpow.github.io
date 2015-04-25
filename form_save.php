ce109ad226fe6ac55068a1e19f87c1b5e5fe2217
Marceau Bernelin <marceau.bernelin@epitech.eu>
2015-04-25 17:38:07 +0200

    commit
    test

--- form_save.php
+++ form_save.php
@@ -6,6 +6,11 @@
      \/ /_/  \__,_|\__, |\___/  \/    \/\__,_|_|  \___\___|\__,_|\__,_|                               
      |___/                                                 -->
 
+<?php
+  include 'class.php';
+  $stats = new lolPow($_GET['region'], strtolower($_GET['player']), '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41');
+?>
+
      <html lang="en">
      <title>LolPow</title>
      <head>
@@ -85,107 +90,21 @@
                   <div class="col-sm-4">
                     <div class="panel panel-primary">
                       <div class="panel-heading">
-                        <h3 class="panel-title">Summary</h3>
+                        <h3 class="panel-title">Red Team</h3>
                       </div>
                       <div class="panel-body">
                         <?php
-
-                        class lolPow
+                        $player = $stats->spectate();
+                        $i = 0;
+                        $halfPlayer = round($player->i / 2) - 1;
+                        while($i != ($halfPlayer + 1))
                         {
-                          public $region;
-                          public $player;
-                          public $api_key;
-                          public $onetwo;
-                          public $onethree;
-                          public $onefour;
-                          public $twotwo;
-                          public $twofour;
-                          public $twofive;
-                          public $summId;
-
-                          public function __construct($region, $player, $api_key)
-                          {
-                            $this->region = $region;
-                            $this->player = $player;
-                            $this->player = str_replace(" ", "", $this->player);
-                            $this->player = str_replace("%20", "", $this->player);
-                            $this->api_key = $api_key;
-                            $this->onetwo = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.2/';
-                            $this->onethree = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.3/';
-                            $this->onefour = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v1.4/';
-                            $this->twotwo = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.2/';
-                            $this->twofour = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.4/';
-                            $this->twofive = 'https://' . $this->region . '.api.pvp.net/api/lol/' . $this->region . '/v2.5/';
-                          }
-
-                          public function askApi($url)
-                          {
-                            $ch = curl_init($url);
-                            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                            $infos = curl_exec($ch);
-                            curl_close($ch);
-                            return $infos;
-                          }
-
-                          public function getSummonerId()
-                          {
-                            $url = $this->onefour . 'summoner/by-name/' . $this->player . $this->api_key;
-                            $data = $this->askApi($url);
-                            $result = json_decode($data);
-                            $player = $this->player;
-                            $this->summId = $result->$player->id;
-                          }
-
-                          public function regionToSpectate()
-                          {
-                            $this->getSummonerId();
-                            switch ($this->region)
-                            {
-                              case 'euw':
-                              $platformId = 'EUW1';
-                              break;
-
-                              case 'na':
-                              $platformId = 'NA1';
-                              break;
-
-                              default:
-                              break;
-                            }
-                            return ('https://' . $this->region . '.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/' . $platformId . '/' . $this->summId . $this->api_key);
-                          }
-
-                          public function spectate()
-                          {
-                            $url = $this->regionToSpectate();
-                            $data = $this->askApi($url);
-                            $result = json_decode($data);
-                            foreach ($result->participants as $name)
-                            {
-                              echo $name->summonerName . " - ";
-                              $champ_data = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region . '/v1.2/champion/' . $name->championId . $this->api_key);
-                              $champ = json_decode($champ_data);
-                              $champ_name = str_replace(" ", "", $champ->name);
-                              $champ_name = str_replace("'", "", $champ_name);
-                              ?>
-                              <img src=<?php echo '/res/champions/' . $champ_name . '_Square_0.png'?> height="36" width"36">
-                              <?php
-                              $summSpell = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region .'/v1.2/summoner-spell/' . $name->spell1Id . $this->api_key);
-                              $summSpell1 = json_decode($summSpell);
-                              $summSpell = $this->askApi('https://global.api.pvp.net/api/lol/static-data/' . $this->region .'/v1.2/summoner-spell/' . $name->spell2Id . $this->api_key);
-                              $summSpell2 = json_decode($summSpell);
-                              ?>
-                              <img src=<?php echo '/res/summoner_spells/' . $summSpell1->name . '.png'?> height="18" width"18">
-                              <img src=<?php echo '/res/summoner_spells/' . $summSpell2->name . '.png'?> height="18" width"18">
-                              <br>
-                              <?php
-                            }
-                          }  
+                          echo $player->summonerName[$i] . " ";
+                          echo '<img src="/res/champions/' . $player->champName[$i] . '_Square_0.png" height="36" width="36">';
+                          echo '<img src="/res/summoner_spells/' . $player->summSpell1[$i] . '.png" height="18" width="18">';
+                          echo '<img src="/res/summoner_spells/' . $player->summSpell2[$i] . '.png" height="18" width="18">' . "<br>";
+                          $i++;
                         }
-
-                        $stats = new lolPow($_GET['region'], strtolower($_GET['player']), '?api_key=c54b731a-fac6-4355-b11b-2c5ee40bea41');
-                        $stats->spectate();
-
                         ?>
                       </div>
                     </div>
@@ -193,7 +112,7 @@
                   <div class="col-sm-4">
                     <div class="panel panel-primary">
                       <div class="panel-heading">
-                        <h3 class="panel-title">Average Division</h3>
+                        <h3 class="panel-title">General Information</h3>
                       </div>
                       <div class="panel-body">
                         Panel content
@@ -203,10 +122,19 @@
                   <div class="col-sm-4">
                     <div class="panel panel-primary">
                       <div class="panel-heading">
-                        <h3 class="panel-title">Bans</h3>
+                        <h3 class="panel-title">Blue Team</h3>
                       </div>
                       <div class="panel-body">
-                        Panel content
+                        <?php
+                        while($i != ($player->i + 1))
+                        {
+                          echo '<img src="/res/summoner_spells/' . $player->summSpell1[$i] . '.png" height="18" width="18">';
+                          echo '<img src="/res/summoner_spells/' . $player->summSpell2[$i] . '.png" height="18" width="18">';
+                          echo '<img src="/res/champions/' . $player->champName[$i] . '_Square_0.png" height="36" width="36">';
+                          echo " " . $player->summonerName[$i] . "<br>";
+                          $i++;
+                        }
+                        ?>
                       </div>
                     </div>
                   </div>
