ea79867ad1586ab3c033f7d34ec93fb4b1bff914
Hugo Villevieille <hugo.villevieille@epitech.eu>
2015-11-06 18:47:12 +0100

    -formulaire modifie, gestion touche enter a faire. -en cliquant sur na et mettre un nom de summoner, generation des tabs

---
+++ js version/js/scouter.js
@@ -0,0 +1,44 @@
+﻿var api_key = "cead18d4-28d4-4c90-8ea6-3cfcc6eaa18b";
+var pill = "<ul class=\"nav nav-pills nav-center\" id=\"myPill\"><li role\"><a class=\"white\" data-toggle=\"tab\" href=\"#ingame\">InGame</a></li><li class=\"active\" role=\"presentation\"><a class=\"white\" data-toggle=\"tab\" href=\"#profile\">Profile</a></li></ul>";
+
+    function get_data() {
+        var region = "";
+        var summoner_name = "";
+
+        if (!$("#euselector").hasClass("active")) {
+            region = "NA";
+        }
+        else if (!$("#naselector").hasClass("active")) {
+            region = "EU";
+        }
+        summoner_name = $('#summonername').val();
+        append_data(region, summoner_name);
+    }
+
+    function append_data(region, summoner_name) {
+        if (summoner_name == "") {
+            console.log("no summoner name speicfied");
+        }
+        else {
+            if (region == "NA") {
+                $.ajax({
+                    url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + api_key,
+                    type: 'GET',
+                    dataType: 'json',
+                    data: {
+
+                    },
+                    success: function (json) {
+                        $("#results").html(pill);
+                        console.log(json);
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown) {
+                        alert("error getting Summoner data!");
+                    }
+                });
+            }
+            else if (region == "EU") {
+                console.log("bientot :p");
+            }
+        }
+    }
