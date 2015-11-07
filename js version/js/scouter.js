c5100f67dc5dc208b165926cdca0ef66518b36fa
Marceau Bernelin <marceau.bernelin@epitech.eu>
2015-11-07 11:14:29 +0100

    Plus que la liste des champions joués en ranked a parser

--- js version/js/scouter.js
+++ js version/js/scouter.js
@@ -4,63 +4,96 @@ var summonerID;
 var region;
 var summoner_name;
 
-    function get_data()
+function get_data()
+{
+	if (!$("#euselector").hasClass("active")) {
+        region = "na";
+    }
+    else if (!$("#naselector").hasClass("active")) {
+        region = "euw";
+    }
+    summoner_name = $('#summonername').val();
+	
+	if (summoner_name == "")
 	{
-		if (!$("#euselector").hasClass("active")) {
-            region = "na";
-        }
-        else if (!$("#naselector").hasClass("active")) {
-            region = "euw";
-        }
-        summoner_name = $('#summonername').val();
-		
-		if (summoner_name == "")
-		{
-			alert("No summoner name specified");
-			return ;
-		}
-		
-		append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + api_key, 1);
+		alert("No summoner name specified");
+		return ;
 	}
 	
-    function append_data(url, mode)
-	{
-		$.ajax({
-			url: url,
-			type: 'GET',
-			dataType: 'json',
-			data: {
+	append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summoner_name + '?api_key=' + api_key, 1);
+}
 
-			},
-			success: function (json) {
-				//$("#results").html(pill);
-				
-				// TODO : Parser les infos, puis les afficher
-				
-				if (mode == 1) // Récupération du summonerID
-				{
-					console.log(json);
-					summonerID = json[summoner_name]["id"];
-					append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.5/league/by-summoner/' + summonerID + '/entry?api_key=' + api_key, 2);
-				}
-				else if (mode == 2) // Stats soloQ et ranked team
-				{
-					console.log(json);
-					append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerID + '/ranked?api_key=' + api_key, 3);
-				}
-				else if (mode == 3) // Champions joués en ranked
-				{
-					console.log(json);
-					append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerID + '/summary?api_key=' + api_key, 4);
-				}
-				else if (mode == 4) // Nombre de win dans chaque mode de jeu
-				{
-					console.log(json);
-				}
-				
-			},
-			error: function (XMLHttpRequest, textStatus, errorThrown) {
-				alert("Error getting summoner data!");
+function append_data(url, mode)
+{
+	$.ajax({
+		url: url,
+		type: 'GET',
+		dataType: 'json',
+		data: {
+
+		},
+		success: function (json) {
+			//$("#results").html(pill);
+			
+			// TODO : Parser les infos, puis les afficher
+			
+			if (mode == 1) // Récupération du summonerID
+			{
+				//console.log(json);
+				summonerID = json[summoner_name]["id"];
+				append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.5/league/by-summoner/' + summonerID + '/entry?api_key=' + api_key, 2);
 			}
-		});
-    }
+			else if (mode == 2) // Stats soloQ et ranked team
+			{
+				//console.log(json);
+				parseRankedStats(json);
+				append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerID + '/ranked?api_key=' + api_key, 3);
+			}
+			else if (mode == 3) // Champions joués en ranked
+			{
+				console.log(json);
+				append_data('https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.3/stats/by-summoner/' + summonerID + '/summary?api_key=' + api_key, 4);
+			}
+			else if (mode == 4) // Nombre de win dans chaque mode de jeu
+			{
+				//console.log(json);
+				parseGlobalStats(json);
+			}
+			
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert("Error getting summoner data!");
+		}
+	});
+}
+
+function parseRankedStats(json)
+{
+	var object = json[summonerID];
+	
+	for (var i = 0; i < object.length; i++)
+	{
+		var playerOrTeamName = object[i]["entries"][0]["playerOrTeamName"];
+		var tier = object[i]["tier"];
+		var division = object[i]["entries"][0]["division"];
+		var queue = object[i]["queue"];
+		var leaguePoints = object[i]["entries"][0]["leaguePoints"];
+		var wins = object[i]["entries"][0]["wins"];
+		var losses = object[i]["entries"][0]["losses"];
+
+		console.log(playerOrTeamName + " " + queue + " " + tier + " " + division + " " + leaguePoints + "LP Wins: " + wins + " Losses: " + losses);
+	}
+}
+
+function parseGlobalStats(json)
+{
+	var object = json["playerStatSummaries"];
+
+	for (var i = 0; i < object.length; i++)
+	{
+		var gameMode = object[i]["playerStatSummaryType"];
+		var wins = object[i]["wins"];
+
+		console.log(gameMode + " Wins: " + wins);
+	}
+}
